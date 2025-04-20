3fed34b27f7d9409f7f7d54091f99398dbb0bc48
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2025-04-20 23:40:46 +0530

    checking MongoDB Database connection

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        .then(() => console.log("MongoDB Database Connected"))
+        .catch(err => console.log("DB connection error:", err));
+
+    } catch (error) {
+        console.log(`MongoDB Connection Failed : ${error}`);
+        process.exit(1)
+    }
+}
+module.exports = connectDB;
